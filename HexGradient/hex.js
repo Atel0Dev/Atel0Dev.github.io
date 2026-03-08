208107a701790e8b4d8c1e666373f726ecd70018
Atel0Dev <nicklaws2003@gmail.com>
2026-03-08 14:39:46 -0400

    Copy to Clipboard
    I added a simple copy to clipboard functionality to select the colors you want and the types of output you would like from them.

--- HexGradient/hex.js
+++ HexGradient/hex.js
@@ -12,6 +12,10 @@ var box1 = document.getElementById("box1");
 var box2 = document.getElementById("box2");
 var box3 = document.getElementById("box3");
 
+let hexCheck = document.getElementById("hexCheck");
+let rgbCheck = document.getElementById("rgbCheck");
+let hsvCheck = document.getElementById("hsvCheck");
+
 //let hex1 = "#000000";
 //let hex2 = "#f700ff";
 
@@ -156,8 +160,6 @@ function rbgGradientHelper(rgb1,rgb2){
 
     res = [];
     
-    //console.log(`1 rgb(R:${rgb1.r} G:${rgb1.g} B:${rgb1.b})\n2 rgb(R:${rgb2.r} G:${rgb2.g} B:${rgb2.b})`);
-
     createdArrayR = createLinearArray(rgb1.r,rgb2.r,sliderCurrentVal);
     console.log(`Created R Array:\n ${createdArrayR}`);
 
@@ -182,7 +184,6 @@ function rbgGradientHelper(rgb1,rgb2){
     }
 
     return res;
-    //return middle color
 }
 
 function hsvGradientHelper(hsv1,hsv2){
@@ -217,43 +218,44 @@ function setupScreen(){
 
     gradientColors = [];
     
-    //gradientHelper(hexToRGB(startingHex),hexToRGB(endingHex));
     currentRGB = rbgGradientHelper(hexToRGB(startingHex),hexToRGB(endingHex));
     currentHSV = hsvGradientHelper(RGBtoHSV(hexToRGB(startingHex)),RGBtoHSV(hexToRGB(endingHex)));
-    let firstRGB = currentRGB[0];
 
+    let firstRGB = currentRGB[0];
     rgb = {
         r: firstRGB.r,
         g: firstRGB.g,
         b: firstRGB.b
     }
-    let firstHSV = currentHSV[0]
 
+    let firstHSV = currentHSV[0]
     hsv = {
         h : firstHSV[0],
         s : firstHSV[1],
         v : firstHSV[2]
     }
-    console.log(RGBtoHex(rgb));
+
 
     rgbBoxParent.style.borderStyle = "solid";
-    console.log(currentRGB);
     
     while(rgbBoxParent.firstChild){
         rgbBoxParent.removeChild(rgbBoxParent.lastChild);
     }
+
     while(hsvBoxParent.firstChild){
         hsvBoxParent.removeChild(hsvBoxParent.lastChild);
     }
+
     //RGB
     for(let i = 0; i < currentRGB.length; i++){
-        currentDiv = document.createElement("div");
+        const currentDiv = document.createElement("div");
 
         currentDiv.className = "colorBox";
+        let copyText = [];
 
-        let firstRGB = currentRGB[i];
+        const firstRGB = currentRGB[i];
 
-        rgb = {
+        const rgb = {
             r: firstRGB.r,
             g: firstRGB.g,
             b: firstRGB.b
@@ -261,17 +263,62 @@ function setupScreen(){
 
         currentDiv.style.backgroundColor = `rgb(${rgb.r},${rgb.g},${rgb.b})`;
 
+        currentDiv.onclick = () => {
+            if(rgbCheck.checked == true){
+                copyText.push(`rgb(${rgb.r},${rgb.g},${rgb.b})`)
+            }
+            if(hexCheck.checked == true){
+                copyText.push(`${RGBtoHex(rgb)}`);
+            }
+            if(hsvCheck.checked == true){
+                const hsvArr = RGBtoHSV(rgb);
+                copyText.push(`hsv(${Math.round(hsvArr[0])}, ${Math.round(hsvArr[1])}%, ${Math.round(hsvArr[2])}%)`);
+            }
+            console.log(copyText.join(' '));
+            if(copyText.length > 0){
+                navigator.clipboard.writeText(copyText.join(' ')).then(() => console.log(`copied to clipboard`)).catch(err => console.error("Failed to copy:", err));
+                while(copyText.length > 0){
+                    copyText.pop();
+                }
+            } else{
+                console.log(`Didnt copy shit`);
+            }
+        }
+
         rgbBoxParent.appendChild(currentDiv);
     }
 
     //HSV
     for(let i = 0; i < currentHSV.length; i++){
-        currentDiv = document.createElement("div");
+        const currentDiv = document.createElement("div");
         currentDiv.className = "colorBox";
-
+        
+        let copyText = [];
         let rgb = HSVtoRGB([currentHSV[i].h,currentHSV[i].s,currentHSV[i].v]);
 
         currentDiv.style.backgroundColor = `rgb(${rgb.r},${rgb.g},${rgb.b})`;
+
+        currentDiv.onclick = () => {
+            if(rgbCheck.checked == true){
+                copyText.push(`rgb(${rgb.r},${rgb.g},${rgb.b})`)
+            }
+            if(hexCheck.checked == true){
+                copyText.push(`${RGBtoHex(rgb)}`);
+            }
+            if(hsvCheck.checked == true){
+                const hsvArr = RGBtoHSV(rgb);
+                copyText.push(`hsv(${Math.round(hsvArr[0])}, ${Math.round(hsvArr[1])}%, ${Math.round(hsvArr[2])}%)`);
+            }
+            console.log(copyText.join(' '));
+            if(copyText.length > 0){
+                navigator.clipboard.writeText(copyText.join(' ')).then(() => console.log(`copied ${copyText}`)).catch(err => console.error("Failed to copy:", err));
+                while(copyText.length > 0){
+                    copyText.pop();
+                }
+            } else{
+                console.log(`Didnt copy shit`);
+            }
+        }
         hsvBoxParent.appendChild(currentDiv);
     }
     //LHC
@@ -284,75 +331,3 @@ function setupScreen(){
         text.style.opacity = "1";
     }, 100);
 }
-
-
-//hex (#ffffff) merge with hex (#000000) should come out as (#808080)
-//ff = 255, 00 = 0, 80 = 128
-//f = 15
-//ff = 15 * 16 + 15 (255)
-//looks normally like (15*16^1) + (15 * 16^0)
-//fff = (15 * 16^2) + (15 * 16) + 15 (4095)
-
-//#80 = 8 * 16 + 0 (128)
-
-
-
-
-
-/*
-
-Lets say I have a list of 10 items in an ordered from least to most digits
-[0,1,2,3,4,5,6,7,8,9]
-
-
-Using 0 and 9 (index[0] and index[9])
-How would I get to 8 (index[8])?
-
-In this instance, just minus 9 by 1, but that wont work if the list is slightly different
-
-So for further security, Im adding more lists
-
-[0,1,2,3,4,5,6,7,8,9]
-[0,3,14,23,25,36,49,68,90,151]
-[0,2,5,13,15,17,20,26,37,50]
-
-Im not sure its actually possible to determine index[8] from index[0] and index[9]
-But, can I generate a list between 2 numbers that fits between those 2 numbers min and max?
-
-Filling in from the 9th index?
-
-I know its going to take 10 steps, to fill the list, Minus the 2 digits preset (index[0] and index[9])
-
-Lets start with the first list
-
-0 -> 9
-Fill an array with size of 10 filling from greatest to least (I guess it doesnt *need* to fill from greatest to least)
-
-((0 + 9) / 10) - 1 = 8
-((0 + 8)) / 10) - 1 = 7
-((0 + 7)) / 10) - 1 = 6
-
-This does seem to work for that list, but lets go with an even larger one.
-
-0 -> 90
-Same rules apply, using past ideas to get:
-
-((0 + 90)) / 10) - 1 = 8
-((0 + 8)) / 10) - 1 = 7
-
-Well, it breaks here.
-
-So I cant use the array size minus the designated slot to find my answer
-
-For 0->90 array size 10 each number should be 10 apart I assume so like,
-[0,10,20,30,40,50,60,70,80,90]
-
-90 - ((90) / 10) = 81
-81 - (81/10) = 73.9 
-
-increment = (highNumber - lowNumber)/(arraySize - 1);
-
-
-
-
-*/
